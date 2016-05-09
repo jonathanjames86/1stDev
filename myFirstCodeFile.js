dd3c7a89cf6b3d42f26b56cdc0d756189ec0bc23
jonathanjames86 <jonathan.putnam@vivint.com>
2016-05-09 14:55:14 -0600

    Update myFirstCodeFile.js

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -1,9 +1,11 @@
 var dm11 = "Let's rock and roll";
 var makeit = "make it happen";
 var tank = "Frank";
+var animal = "dog"
 
 var journal = [];
 
 journal.push(dm11);
 journal.push(makeit);
 journal.push(tank);
+journal.push(animal)
